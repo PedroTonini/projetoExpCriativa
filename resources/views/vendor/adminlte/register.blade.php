d8de66636d648b572015a441a5825f572bb8e645
Pedro Tonini <toninipedro46@gmail.com>
2020-05-30 15:28:07 -0300

    Customização front Login e Register

--- resources/views/vendor/adminlte/register.blade.php
+++ resources/views/vendor/adminlte/register.blade.php
@@ -89,12 +89,12 @@
                             </div>
                         @endif
                     </div>
-                    <button type="submit" class="btn btn-primary btn-block btn-flat">
+                    <button type="submit" class="btn btn-rosaChock btn-block btn-flat">
                         {{ __('adminlte::adminlte.register') }}
                     </button>
                 </form>
                 <p class="mt-2 mb-1">
-                    <a href="{{ $login_url }}">
+                    <a href="{{ $login_url }}" class="txt-rosaChock">
                         {{ __('adminlte::adminlte.i_already_have_a_membership') }}
                     </a>
                 </p>
