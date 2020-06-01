6feb670ec896d878355080607c6e9f538016c74f
Lucas Terres <ldvtlucas@hotmail.com>
2020-06-01 16:19:41 -0300

    fix: you dont have required role [admin] on new login

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -14,7 +14,7 @@ class AdminController extends Controller
     }
 
     public function auth() {
-        if ( null !== Auth::user() && Auth::user()->hasRole('admin') ) {
+        if ( null !== Auth::user() && !Auth::user()->hasRole('admin') ) {
             Auth::logout();
         }
         return redirect('/admin/home');
--- resources/views/admin/avaliacoes/dashboard.blade.php
+++ resources/views/admin/avaliacoes/dashboard.blade.php
@@ -9,116 +9,120 @@
 @stop
 
 @section('content')
-
-<div align="center">
-  <div class="small-box bg-primary col-6">
-    <h3><strong>Overview</strong></h3>
-    <h3>{{ number_format($avgAval, '1', '.', ',') }}★</h3>
-  </div>
-</div>
-
-
-<div class="row">
-  <div class="column">
-    <div align="center" id="divAtend">
-      <h3><strong>Atendimento</strong> </h3>
-      <div class="col-lg-6 col-12 divNegativa">
-        <div class="small-box bg-success">
-          <div class="inner">
-            <h5>Ótimas</h5>
-            <h3>{{ $avalAtendBoas }}</h3>
-            <h4>(4+★)</h4>
-          </div>
-        </div>
+ <div class="container">
+   <div class="box">
+    <div align="center">
+      <div class="small-box bg-primary col-6">
+        <h3><strong>Overview</strong></h3>
+        <h3>{{ number_format($avgAval, '1', '.', ',') }}★</h3>
       </div>
-      <div class="col-lg-6 col-12 divPositiva">
-        <div class="small-box bg-danger">
-          <div class="inner">
-            <h5>Ruins</h5>
-            <h3>{{ $avalAtendRuins}}</h3>
-            <h4>(2-★)</h4>
+    </div>
+    
+    
+    <div class="row">
+      <div class="column">
+        <div align="center" id="divAtend">
+          <h3><strong>Atendimento</strong> </h3>
+          <div class="col-lg-6 col-12 divNegativa">
+            <div class="small-box bg-success">
+              <div class="inner">
+                <h5>Ótimas</h5>
+                <h3>{{ $avalAtendBoas }}</h3>
+                <h4>(4+★)</h4>
+              </div>
+            </div>
           </div>
-        </div>
-      </div>
-    </div>    
-  </div>
-
-
-  <div class="column">
-    <div align="center" id="divProdutos">
-      <h3><strong>Produtos</strong></h3>
-      <div class="col-lg-6 col-12 divNegativa border-left border-secondary">
-        <div class="small-box bg-success">
-          <div class="inner">
-            <h5>Ótimas</h5>
-            <h3>{{ $avalProdBoas }}</h3>
-            <h4>(4+★)</h4>
+          <div class="col-lg-6 col-12 divPositiva">
+            <div class="small-box bg-danger">
+              <div class="inner">
+                <h5>Ruins</h5>
+                <h3>{{ $avalAtendRuins}}</h3>
+                <h4>(2-★)</h4>
+              </div>
+            </div>
           </div>
-        </div>
+        </div>    
       </div>
-      <div class="col-lg-6 col-12 divPositiva ">
-        <div class="small-box bg-danger">
-          <div class="inner">
-            <h5>Ruins</h5>
-            <h3>{{ $avalProdRuins }}</h3>
-            <h4>(2-★)</h4>
+    
+    
+      <div class="column">
+        <div align="center" id="divProdutos">
+          <h3><strong>Produtos</strong></h3>
+          <div class="col-lg-6 col-12 divNegativa border-left border-secondary">
+            <div class="small-box bg-success">
+              <div class="inner">
+                <h5>Ótimas</h5>
+                <h3>{{ $avalProdBoas }}</h3>
+                <h4>(4+★)</h4>
+              </div>
+            </div>
+          </div>
+          <div class="col-lg-6 col-12 divPositiva ">
+            <div class="small-box bg-danger">
+              <div class="inner">
+                <h5>Ruins</h5>
+                <h3>{{ $avalProdRuins }}</h3>
+                <h4>(2-★)</h4>
+              </div>
+            </div>
           </div>
         </div>
       </div>
     </div>
-  </div>
-</div>
-
-
-<div class="row">
-  <div class="column">
-    <div align="center" id="divPreco">
-      <h3><strong>Preço</strong></h3>
-      <div class="col-lg-6 col-12 divNegativa">
-        <div class="small-box bg-success">
-          <div class="inner">
-            <h5>Ótimas</h5>
-            <h3>{{ $avalPrecoBoas }}</h3>
-            <h4>(4+★)</h4>
+    
+    
+    <div class="row">
+      <div class="column">
+        <div align="center" id="divPreco">
+          <h3><strong>Preço</strong></h3>
+          <div class="col-lg-6 col-12 divNegativa">
+            <div class="small-box bg-success">
+              <div class="inner">
+                <h5>Ótimas</h5>
+                <h3>{{ $avalPrecoBoas }}</h3>
+                <h4>(4+★)</h4>
+              </div>
+            </div>
           </div>
-        </div>
-      </div>
-      <div class="col-lg-6 col-12 divPositiva">
-        <div class="small-box bg-danger">
-          <div class="inner">
-            <h5>Ruins</h5>
-            <h3>{{ $avalPrecoRuins}}</h3>
-            <h4>(2-★)</h4>
+          <div class="col-lg-6 col-12 divPositiva">
+            <div class="small-box bg-danger">
+              <div class="inner">
+                <h5>Ruins</h5>
+                <h3>{{ $avalPrecoRuins}}</h3>
+                <h4>(2-★)</h4>
+              </div>
+            </div>
           </div>
-        </div>
+        </div>    
       </div>
-    </div>    
-  </div>
-
-
-  <div class="column">
-    <div align="center" id="divMarca">
-      <h3><strong>Marcas</strong></h3>
-      <div class="col-lg-6 col-12 divNegativa border-left border-secondary">
-        <div class="small-box bg-success">
-          <div class="inner">
-            <h5>Ótimas</h5>
-            <h3>{{ $avalMarcasBoas }}</h3>
-            <h4>(4+★)</h4>
+    
+    
+      <div class="column">
+        <div align="center" id="divMarca">
+          <h3><strong>Marcas</strong></h3>
+          <div class="col-lg-6 col-12 divNegativa border-left border-secondary">
+            <div class="small-box bg-success">
+              <div class="inner">
+                <h5>Ótimas</h5>
+                <h3>{{ $avalMarcasBoas }}</h3>
+                <h4>(4+★)</h4>
+              </div>
+            </div>
           </div>
-        </div>
-      </div>
-      <div class="col-lg-6 col-12 divPositiva ">
-        <div class="small-box bg-danger">
-          <div class="inner">
-            <h5>Ruins</h5>
-            <h3>{{ $avalMarcasRuins }}</h3>
-            <h4>(2-★)</h4>
+          <div class="col-lg-6 col-12 divPositiva ">
+            <div class="small-box bg-danger">
+              <div class="inner">
+                <h5>Ruins</h5>
+                <h3>{{ $avalMarcasRuins }}</h3>
+                <h4>(2-★)</h4>
+              </div>
+            </div>
           </div>
         </div>
       </div>
     </div>
-  </div>
-</div>
+   </div>
+ </div>
+
 
 @stop
