96c54c3ef25d28a472a9c49b522ad0f6d6d71f7e
José Dias <josegalvesdias@gmail.com>
2020-06-01 19:56:08 -0300

    add validade promocoes

--- resources/views/admin/promocoes/criar.blade.php
+++ resources/views/admin/promocoes/criar.blade.php
@@ -12,8 +12,17 @@
     <form action="{{ URL::route('promocoes.store') }}" method="POST">
     {!! csrf_field() !!}
         <div class="col-lg-6 col-12 container">
+        Insira o nome da promoção:
         <input class="form-control" type="text" name="nome" placeholder="Nome da promoção" required autofocus > <br>
+        
+        Insira o valor da promoção:
         <input class="form-control" type="textarea" name="valor" placeholder="Valor da promoção" required autofocus> <br>
+        
+        Insira a data de validade final da promoção:
+        <input class="form-control" type="date" name="vencimento" placeholder="mm/dd/aaaa"> <br>
+            
+
+        Insira o código da promoção:
         <input class="form-control" type="text" name="codigo" placeholder="Codigo da promoção" required autofocus> <br>
         <button class="form-control btn-primary" type="submit">Cadastrar</button>
         </div>
