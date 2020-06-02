14178de372c66d8b53b6e32dcaba0d16f3a885d0
Lucas Terres <ldvtlucas@hotmail.com>
2020-06-02 19:42:51 -0300

    fix: avaliacoes individuais dando erro no SQL

--- app/Models/AvaliacaoDeCompra.php
+++ app/Models/AvaliacaoDeCompra.php
@@ -9,8 +9,7 @@ use App\Models\User;
 class AvaliacaoDeCompra extends Model
 {
     public function getName(){
-        $user_id = Cliente::find($this->cliente_id)->user_id;
-        $nome = User::find($user_id)->name;
+        $nome = User::find($this->user_id)->name;
         return $nome;
     }
 
